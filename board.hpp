e1d80b7bf0f661c2b49777005f98f750ffbbd7a0
Nicolás Santiago Gómez Forján <nicogomfor@gmail.com>
2023-12-24 18:29:58 +0100

    Split verifyMovement() into various funct for each piece

--- board.hpp
+++ board.hpp
@@ -1,12 +1,15 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 #include <array>
-#include "bscurses.hpp"
 #include "piece.hpp"
+#include "bscurses.hpp"
+
+#define BOARD_SIZE 8
+#define BOARD std::array<std::array<piece*, BOARD_SIZE>, BOARD_SIZE>
 
 // Since we are working with ncurses, first one is y and second one is x. I hate
 // it, but I'd rather not have two coordinate systems at the same time
 
-extern void initBoard(std::array<std::array<piece*, 8>, 8> &_board);
-extern void drawBoard(std::array<std::array<piece*, 8>, 8> &_board);
+extern void initBoard(BOARD &_board);
+extern void drawBoard(BOARD &_board);
 #endif
--- verify.cpp
+++ verify.cpp
@@ -1,196 +1,225 @@
 #include "verify.hpp"
-#include <exception>
-#include <stdexcept>
 
-void verifyMovement(movement &__cord, std::array<std::array<piece*,8>,8> &__board)
+bool verifyPawn(movement &___cord, BOARD &___board)
+{
+    if(___cord.isCapture==true)
+    {
+        //Capture logic. Unimplemented for now
+        throw -1;
+    }
+    else if (___board.at(___cord.to.at(0)-2).at(___cord.to.at(1)) == pawnptr && ___cord.to.at(0)-2 == 1) 
+    {
+        ___cord.from.at(0)=___cord.to.at(0)-2;
+        ___cord.from.at(1)=___cord.to.at(1);
+        return true;
+
+    }
+    else if (___board.at(___cord.to.at(0)-1).at(___cord.to.at(1)) == pawnptr) 
+    {
+        ___cord.from.at(0)=___cord.to.at(0)-1;
+        ___cord.from.at(1)=___cord.to.at(1);
+        return true;
+    }
+    return false;            
+}
+
+bool verifyKnight(movement &___cord, BOARD &___board)
+{
+    // I don't have a better idea
+    for(auto ref=0; ref<=8; ref++)
+    {
+        short y,x;
+        //Switch case for switching the y and x values based on ref
+        switch (ref) 
+        {
+            case 0:
+                y=1;
+                x=2;
+                break;
+            case 1:
+                y=-1;
+                x=-2;
+            break;
+            case 2:
+                y=2;
+                x=1;
+                break;
+            case 3:
+                y=-2;
+                x=-1;
+                break;
+            case 4:
+                y=1;
+                x=-2;
+                break;
+            case 5:
+                y=-1;
+                x=2;
+                break;
+            case 6:
+                y=2;
+                x=-1;
+                break;
+            case 7:
+                y=-2;
+                x=1;
+                break;
+            default:
+                throw -1;
+                break;
+        }
+        try 
+        {
+            if(___board.at(___cord.to.at(0)+y).at(___cord.to.at(1)+x) == knightptr)
+            {
+            ___cord.from.at(0)=___cord.to.at(0)+y;
+            ___cord.from.at(1)=___cord.to.at(1)+x;
+            return true;
+            }
+        } 
+        catch (...) 
+        {
+        //Do nothing because we already "handle" it by increasing ref
+        }
+        
+    }
+    return false;
+}
+
+bool verifyBishop(movement &___cord, BOARD &___board)
+{
+    for (int ref=0; ref<=3; ref++)
+    {
+        //Lambda funtion for switching states (going foward or backwards) based on the iteration and if it is the first or second for loop
+        auto refSwapper = [ref](bool isY)
+        {
+            switch(ref)
+            {
+                case 0:
+                    return 1;
+                break;
+
+                case 1:
+                    return -1;
+                break;
+
+                case 2:
+                    return isY == true ? 1 : -1;
+                break;
+
+                case 3:
+                    return isY == true ? -1 : 1;
+                break;
+                
+                default:
+                    throw std::runtime_error("Lambda refSwapper couldn't find a value. Go complain in the Git repo.");
+                
+            }
+        };
+        
+        try 
+        {
+            for(int i=0; i<=8; i++)
+            {
+                int y=___cord.to.at(0)+i*refSwapper(true);
+                int x=___cord.to.at(1)+i*refSwapper(false);
+
+                
+                
+                    if(___board.at(y).at(x) == bishopptr) // && __board.at(i).at(j)->colour==="the same colour than the current player"
+                    {
+                        ___cord.from.at(0)=y;
+                        ___cord.from.at(1)=x;
+                        return true;
+                    }
+                    else if (___board.at(y).at(x) != emptyptr) 
+                    {
+                        break;
+                    }
+            } 
+        }
+        //This probably bad design
+        catch (...) 
+        {
+            //Do nothing because errors are already "handled" by just quitting
+        } 
+                
+    }
+
+    return false;
+}
+
+bool verifyRook(movement &___cord, BOARD &___board)
+{
+    for (int ref=0; ref<=1; ref++)
+    {
+        for(int x=___cord.to.at(1); x<=7; x+=(ref == 0 ? 1 : -1))
+        {
+            if (x<0) 
+            {
+                break;
+            }
+            if(___board.at(___cord.to.at(0)).at(x) == rookptr) // && __board.at(i).at(j)->colour==="the same colour than the current player"
+            {
+                ___cord.from.at(0)=___cord.to.at(0);
+                ___cord.from.at(1)=x;
+                return true;
+            }
+            else if (___board.at(___cord.to.at(0)).at(x) != emptyptr) 
+            {
+                break;
+            }
+        }
+
+        for(int y=___cord.to.at(0); y<=7; y+=(ref == 0 ? 1 : -1))
+        {
+            if (y<0) 
+            {
+                break;
+            }
+            if(___board.at(y).at(___cord.to.at(1)) == rookptr) // && __board.at(i).at(j)->colour==="the same colour than the current player"
+            {
+                ___cord.from.at(0)=y;
+                ___cord.from.at(1)=___cord.to.at(1);
+                return true;
+            }
+            else if (___board.at(y).at(___cord.to.at(1)) != emptyptr) 
+            {
+                break;
+            }
+        }
+                
+    }
+    
+    return false;
+}
+
+void verifyMovement(movement &__cord, BOARD &__board)
 {
     try 
     {
+        bool valid=false;
+
         switch(__cord.type)
         {
             
+            
             case PAWN_ID:
-                if(__cord.isCapture==true)
-                {
-                    //Capture logic. Unimplemented
-                    throw -1;
-                }
-                else if (__board.at(__cord.to.at(0)-2).at(__cord.to.at(1)) == pawnptr && __cord.to.at(0)-2 == 1) 
-                {
-                    __cord.from.at(0)=__cord.to.at(0)-2;
-                    __cord.from.at(1)=__cord.to.at(1);
-
-                }
-                else if (__board.at(__cord.to.at(0)-1).at(__cord.to.at(1)) == pawnptr) 
-                {
-                    __cord.from.at(0)=__cord.to.at(0)-1;
-                    __cord.from.at(1)=__cord.to.at(1);
-                }
-                break;
+                valid = verifyPawn(__cord, __board);
+            break;
+                
                 
             case KNIGHT_ID:
-            // I don't have a better idea
-            for(auto ref=0; ref<=8; ref++)
-            {
-                short y,x;
-                //Switch case for switching the y and x values based on ref
-                switch (ref) 
-                {
-                    case 0:
-                        y=1;
-                        x=2;
-                        break;
-                    case 1:
-                        y=-1;
-                        x=-2;
-                    break;
-                    case 2:
-                        y=2;
-                        x=1;
-                        break;
-                    case 3:
-                        y=-2;
-                        x=-1;
-                        break;
-                    case 4:
-                        y=1;
-                        x=-2;
-                        break;
-                    case 5:
-                        y=-1;
-                        x=2;
-                        break;
-                    case 6:
-                        y=2;
-                        x=-1;
-                        break;
-                    case 7:
-                        y=-2;
-                        x=1;
-                        break;
-                    default:
-                        throw -1;
-                        break;
-                }
-                try 
-                {
-                    if(__board.at(__cord.to.at(0)+y).at(__cord.to.at(1)+x) == knightptr)
-                    {
-                    __cord.from.at(0)=__cord.to.at(0)+y;
-                    __cord.from.at(1)=__cord.to.at(1)+x;
-                    break;
-                    }
-                } 
-                catch (...) 
-                {
-                //Do nothing because we already "handle" it
-                }
-                
-            }
+                valid = verifyKnight(__cord, __board);
             break;
 
             case BISHOP_ID:
-                for (int ref=0; ref<=3; ref++)
-                {
-                    //Lambda funtion for switching states (going foward or backwards) based on the iteration and if it is the first or second for loop
-                    auto refSwapper = [ref](bool isY)
-                    {
-                        switch(ref)
-                        {
-                            case 0:
-                                return 1;
-                            break;
-
-                            case 1:
-                                return -1;
-                            break;
-
-                            case 2:
-                                return isY == true ? 1 : -1;
-                            break;
-
-                            case 3:
-                                return isY == true ? -1 : 1;
-                            break;
-                            
-                            default:
-                                throw std::runtime_error("Lambda refSwapper couldn't find a value. Go complain in the Git repo.");
-                            
-                        }
-                    };
-                    
-                    try 
-                    {
-                        for(int i=0; i<=8; i++)
-                        {
-                            int y=__cord.to.at(0)+i*refSwapper(true);
-                            int x=__cord.to.at(1)+i*refSwapper(false);
-
-                            
-                            
-                                if(__board.at(y).at(x) == bishopptr) // && __board.at(i).at(j)->colour==="the same colour than the current player"
-                                {
-                                    __cord.from.at(0)=y;
-                                    __cord.from.at(1)=x;
-                                    return;
-                                }
-                                else if (__board.at(y).at(x) != emptyptr) 
-                                {
-                                    break;
-                                }
-                        } 
-                    }
-                    //This probably bad design
-                    catch (...) 
-                    {
-                        //Do nothing because errors are already "handled" by just quitting
-                    } 
-                            
-                }
-
+                valid = verifyBishop(__cord, __board);
             break;
 
             case ROOK_ID:
-                for (int ref=0; ref<=1; ref++)
-                {
-                    for(int x=__cord.to.at(1); x<=7; x+=(ref == 0 ? 1 : -1))
-                    {
-                        if (x<0) 
-                        {
-                            break;
-                        }
-                        if(__board.at(__cord.to.at(0)).at(x) == rookptr) // && __board.at(i).at(j)->colour==="the same colour than the current player"
-                        {
-                            __cord.from.at(0)=__cord.to.at(0);
-                            __cord.from.at(1)=x;
-                            return;
-                        }
-                        else if (__board.at(__cord.to.at(0)).at(x) != emptyptr) 
-                        {
-                            break;
-                        }
-                    }
-
-                    for(int y=__cord.to.at(0); y<=7; y+=(ref == 0 ? 1 : -1))
-                    {
-                        if (y<0) 
-                        {
-                            break;
-                        }
-                        if(__board.at(y).at(__cord.to.at(1)) == rookptr) // && __board.at(i).at(j)->colour==="the same colour than the current player"
-                        {
-                            __cord.from.at(0)=y;
-                            __cord.from.at(1)=__cord.to.at(1);
-                            return;
-                        }
-                        else if (__board.at(y).at(__cord.to.at(1)) != emptyptr) 
-                        {
-                            break;
-                        }
-                    }
-                            
-                }
-                break;
+                valid = verifyRook(__cord, __board);
+            break;
 
 
             default:
@@ -201,7 +230,15 @@ void verifyMovement(movement &__cord, std::array<std::array<piece*,8>,8> &__boar
         //Check whether the the destination is empty, the same colour or if it has a different colour (and if it is a capture)
         if(__board.at(__cord.from.at(0)).at(__cord.from.at(1))->colour == __board.at(__cord.to.at(0)).at(__cord.to.at(1))->colour && __board.at(__cord.to.at(0)).at(__cord.to.at(1)) != emptyptr)
         {
-            throw std::runtime_error("Overlapping pieces");
+            valid = false;
+        }
+
+        if (valid == false)
+        {
+            drawBoard(__board);
+            printw("Invalid movement!\n");
+            getCord(__cord, __board);
+            verifyMovement(__cord, __board);
         }
 
         return;
